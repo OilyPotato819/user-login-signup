ec8b2e9f88178fd72644bcb3a86d638a230b9f5d
Oliver Schneider <o.schneider@share.epsb.ca>
2023-03-17 14:49:18 -0600

    started

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 // USER LOGIN / SIGNUP
 
 // HTML VARIABLES
-let signInBtn = document.getElementById('sign-in-btn');
-let signUpBtn = document.getElementById('sign-up-btn');
+let signInBtn = document.getElementById("sign-in-btn");
+let signUpBtn = document.getElementById("sign-up-btn");
+
+let iUsernameInput = document.getElementById("sign-in-username");
+let iPasswordInput = document.getElementById("sign-in-password");
+let uUsernameInput = document.getElementById("sign-up-username");
+let uPasswordInput = document.getElementById("sign-up-password");
+let uConfirmInput = document.getElementById("sign-up-confirm");
 
 // SIGN UP BTN CLICKED
-signUpBtn.addEventListener('click', signUpHandler);
+signUpBtn.addEventListener("click", signUpHandler);
 
 function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
+  console.log();
 }
 
 // SIGN IN BTN CLICKED
-signInBtn.addEventListener('click', signInHandler);
+signInBtn.addEventListener("click", signInHandler);
 
 function signInHandler() {
-  console.log('Sign In Btn Clicked');
+  console.log("Sign In Btn Clicked");
 }
