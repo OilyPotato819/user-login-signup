82276f1a3d7340ce870b18f757ef2aafd447f358
Oliver Schneider <o.schneider@share.epsb.ca>
2023-03-20 14:01:36 -0600

    done

--- js/main.js
+++ js/main.js
@@ -10,16 +10,49 @@ let uUsernameInput = document.getElementById("sign-up-username");
 let uPasswordInput = document.getElementById("sign-up-password");
 let uConfirmInput = document.getElementById("sign-up-confirm");
 
+// GLOBAL VARIABLES
+let members = JSON.parse(localStorage.getItem("members")) || [];
+
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener("click", signUpHandler);
 
 function signUpHandler() {
-  console.log();
+  const username = uUsernameInput.value;
+  const password = uPasswordInput.value;
+  const confirm = uConfirmInput.value;
+
+  if (!(username && password && confirm)) return alert("Please fill out all required fields");
+  if (password != confirm) return alert("Password and Confirm Password do not match");
+  if (getMemberByUsername(username) != -1) return alert("Username is already in use");
+
+  members.push({ username: username, password: password });
+  alert("Account creation successful");
+  save();
 }
 
 // SIGN IN BTN CLICKED
 signInBtn.addEventListener("click", signInHandler);
 
 function signInHandler() {
-  console.log("Sign In Btn Clicked");
+  const username = iUsernameInput.value;
+  const password = iPasswordInput.value;
+  const index = getMemberByUsername(username);
+
+  if (index == -1) return alert("No account found with that username");
+  if (members[index].password != password) return alert("Incorrect password");
+
+  alert("Sign in successful");
+}
+
+// FUNCTIONS
+function save() {
+  localStorage.setItem("members", JSON.stringify(members));
+}
+
+function getMemberByUsername(username) {
+  for (let i = 0; i < members.length; i++) {
+    if (members[i].username === username) return i;
+  }
+
+  return -1;
 }
